3186acaa000fa6a0381ce2ff81d5cc5ecca7c20e
AJ <ancheetah@users.noreply.github.com>
2021-06-04 19:42:40 -0400

    MUI global theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
 import React from 'react';
 import './App.css';
 
+// import "@fontsource/commissioner/400.css";
+// import "@fontsource/commissioner/500.css";
+// import "@fontsource/commissioner/700.css";
+
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import Header from './components/HeaderComponent';
 import Main from './components/MainComponent';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: 'hsl(0, 0%, 48%)',
+      main: 'hsl(176, 50%, 47%)',
+      contrastText: '#ffffff'
+    }
+  },
+  typography: {
+    fontSize: 16,
+    fontFamily: [
+      'Commissioner',
+      'monospace'
+    ].join(',')
+  }
+});
+
+theme.props = {
+  MuiButton: {
+    disableElevation: true
+  }
+}
+
+theme.overrides = {
+  MuiButton: {
+    root: {
+      borderRadius: 50,
+      textTransform: 'none'
+    }
+  },
+  MuiLinearProgress: {
+    root: {
+      height: 10,
+      borderRadius: 50
+    }
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main />
+      <ThemeProvider theme={theme}>
+        <Header/>
+        <Main />
+      </ThemeProvider>
     </div>
   );
 }
