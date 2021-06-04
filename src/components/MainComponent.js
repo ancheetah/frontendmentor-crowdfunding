1b5336c36f78fc4b8e6995694dc67809f60e38f7
AJ <ancheetah@users.noreply.github.com>
2021-06-03 23:43:16 -0400

    added content: project info and progress

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,22 +1,40 @@
 import React from 'react';
-import { Container, Card, CardContent, Button, IconButton } from '@material-ui/core';
+import { Container, Card, CardContent, Button, 
+    Avatar, LinearProgress } from '@material-ui/core';
 import BookmarkIcon from '@material-ui/icons/Bookmark';
 
 export default function Main() {
     return (
         <Container maxWidth="xs">
+
+            {/* Project Info */}
             <Card variant="outlined">
-                <h3>Mastercraft Bamboo Monitor Riser</h3>
+                <Avatar alt="Mastercraft Logo" src="/images/logo-mastercraft.svg" />
+                <h2>Mastercraft Bamboo Monitor Riser</h2>
                 <CardContent>
                     A beautifully handcrafted monitor stand to reduce neck and eye strain.
                 </CardContent>
                 <Button variant="contained" color="primary" disableElevation>
                     Back this project
                 </Button>
-                <IconButton color="secondary">
+                <Button variant="contained" color="default">
                     <BookmarkIcon/>
-                </IconButton>
+                </Button>
             </Card>
+
+            {/* Project Progress */}
+            <Card variant="outlined">
+                <h1>$89,914</h1>
+                <p>of $100,000 backed</p>
+                <hr />
+                <h1>5,007</h1>
+                <p>total backers</p>
+                <hr />
+                <h1>56</h1>
+                <p>days left</p>
+                <LinearProgress variant="determinate" value={ 89914 * 100 / 100000 }/>
+            </Card>
+
         </Container>
     );
 }
